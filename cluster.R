531ef7555b234cd1f1e4d0f9d398971862a8342f
danlee2002 <danlee@gatech.edu>
2023-01-26 14:00:27 -0500

    dataset

--- cluster.R
+++ cluster.R
@@ -1,2 +1,3 @@
-#loads required libraries
-pacman::p_load(psych, Seurat, dplyr)
+pacman::p_load(dplyr, Seurat, patchwork)
+pbmc.data <- Read10X(data.dir = "../data/pbmc3k/filtered_gene_bc_matrices/hg19/")
+pbmc <- CreateSeuratObject(counts = pbmc.data, project = "pbmc3k", min.cells = 3, min.features = 200)
