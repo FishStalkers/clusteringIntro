436d9d5571bc43b7d39c3d928b9ad66d296cc098
danlee2002 <danlee@gatech.edu>
2023-02-07 03:01:01 -0500

    a

--- mouseData.R
+++ mouseData.R
@@ -1,2 +1,16 @@
 pacman::p_load(dplyr, Seurat, patchwork)
-pbmc.data <- Read10X(data.dir = "filtered_gene_bc_matrices/hg19/")
+#reads data 
+pbmc.data <- Read10X(data.dir = "raw_gene_bc_matrices/mm10/")
+mice_data <- CreateSeuratObject(counts = pbmc.data, project  ="mice_data", 
+                                min.cells = 3, min.features = 200)
+head(mice_data)
+        mice_data[["percent.mt"]] <- PercentageFeatureSet(mice_data, pattern = "^mt-")
+#plotting out features for filting
+VlnPlot(mice_data, features = c("nFeature_RNA", "nCount_RNA","percent.mt"), 
+        ncol = 3)
+#plot out distribution of data
+plot1 <- FeatureScatter(mice_data, feature1 = "nCount_RNA", feature2 = "percent.mt")
+plot2 <- FeatureScatter(mice_data, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+plot1 + plot2
+#filter out outliers
+mice_data <- subset(mice_data, subset = nFeature_RNA > 200 & ncount_RNA < 25000 & percent.mt < 5)
