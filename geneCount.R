b397e1aec98b8ceee94a9c19b4b7f368d4de7b0d
danlee2002 <danlee@gatech.edu>
2023-02-07 13:36:21 -0500

    got count

--- geneCount.R
+++ geneCount.R
@@ -3,10 +3,9 @@ pacman::p_load(dplyr, Seurat, patchwork, data.table)
 pbmc.data <- Read10X(data.dir = "raw_gene_bc_matrices/mm10/")
 mice_data <- CreateSeuratObject(counts = pbmc.data, project  ="mice_data", 
                                 min.cells = 3, min.features = 200)
-head(mice_data)
-mice_data[["percent.mt"]] <- PercentageFeatureSet(mice_data, pattern = 
-                                                    "^mt-")
+head(rownames(mice_data))
+head(colnames(mice_data))
+mice_data[["percent.mt"]] <- PercentageFeatureSet(mice_data, pattern = "^mt-")
+mice_datacsv <- GetAssayData(object = mice_data, slot ='counts')
+fwrite(as.data.table(mice_datacsv, ))
 dim(mice_data)
-expr_raw <- GetAssayData(objeg)
-mice_data[rownames(mice_data)]
-dataValue <- as.data.frame(as.matrix(mice_data@scale.data)) 
