4f8ebdecefdc569bc388982d26b737cba14043d4
adam_bhavi <67218242+abhavnag3@users.noreply.github.com>
2023-04-03 17:07:07 -0400

    im slowly losing my mind
    error metricc for just PCA. Most base code from Daniel from mouseData.R file

---
+++ error_metrics.R
@@ -0,0 +1,99 @@
+pacman::p_load(dplyr, Seurat, patchwork)
+#reads data 
+mice.data <- Read10X(data.dir = "raw_gene_bc_matrices/mm10/")
+mice_data <- CreateSeuratObject(counts = mice.data, project  ="mice_data", 
+                                min.cells = 3, min.features = 200)
+head(mice_data)
+mice_data[["percent.mt"]] <- PercentageFeatureSet(mice_data, pattern = 
+                                                    "^mt-")
+head(rownames(mice_data)) 
+head(mice_data["Xkr4" ]["nCount_RNA"])
+#plotting out features for filtering
+VlnPlot(mice_data, features = c("nFeature_RNA", "nCount_RNA","percent.mt"), 
+        ncol = 3)
+#plot out distribution of data
+plot1 <- FeatureScatter(mice_data, feature1 = "nCount_RNA", feature2 = 
+                          "percent.mt", pts.size = 0.5)
+plot2 <- FeatureScatter(mice_data, feature1 = "nCount_RNA", feature2 = 
+                          "nFeature_RNA",pts.size = 0.5,  plot.cor = TRUE)
+plot1 + plot2
+#filter out outliars
+mice_data <- subset(mice_data, subset = nFeature_RNA > 200 & nCount_RNA < 25000 & percent.mt < 5)
+mice_data_n <- NormalizeData(mice_data, normalization.method = "LogNormalize")
+mice_data <- FindVariableFeatures(mice_data, selection.method = "vst", nfeatures = 2000)
+top10 <- head(VariableFeatures(mice_data),10)
+top10
+#finds variable features
+plot1 <- VariableFeaturePlot(mice_data)
+plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
+#plot1 + plot2
+plot1
+#scales and normalize data
+all.genes <- rownames(mice_data)
+mice_data <- ScaleData(mice_data)
+#reduce data 
+mice_data <- RunPCA(mice_data, features = VariableFeatures(object = mice_data))
+print(mice_data[["pca"]], dims = 1:2, nfeatures = 14)
+library(uwot)
+library(Rtsne)
+pc_var <- mice_data@reductions$pca_sdev^2
+pc_mat <- mice_data@reductions$pca@cell.embeddings[,1:50]
+pc_mean <- colMeans(pc_mat)
+pc_dev <- sweep(pc_mat, 2, pc_mean, "-")
+pc_var <- apply(pc_dev, 2, var)
+pc_sd <- sqrt(pc_var)
+var_exp <- pc_var / sum(pc_var)
+sum(var_exp)
+library(ggplot2)
+
+df <- data.frame(PC = 1:length(var_exp), Variance = var_exp)
+scree_1_plot <- ggplot(df, aes(x = PC, y = Variance)) + 
+  geom_col(fill = "steelblue") + 
+  scale_x_continuous(breaks = seq(0, 50, by = 5)) + 
+  scale_y_continuous(limits = c(0, 1), expand = c(0, 0)) + 
+  labs(x = "Principal Component", y = "Percentage of Variance Explained")
+cumulative_var_exp <- cumsum(var_exp)
+library(ggplot2)
+scree_plot <- ggplot(data.frame(x=1:length(var_exp), y=cumulative_var_exp), aes(x=x, y=y)) +
+  geom_line() +
+  geom_point() +
+  scale_x_continuous(name="Principal Component") +
+  scale_y_continuous(name="Cumulative Variance Explained") +
+  theme_minimal()
+scree_plot + scree_1_plot
+
+
+
+
+#visualizes impact of each gene
+VizDimLoadings(mice_data, dims = 1:2, nfeatures = 14, reduction = "pca")
+#dim plot
+
+DimPlot(mice_data, reduction ="pca")
+DimHeatmap(mice_data, dims = 1, cells = 500, balanced = TRUE)
+#elbow method 
+ElbowPlot(mice_data)
+#Clusterring
+mice_data <- FindNeighbors(mice_data, dims = 1:13)
+mice_data <- FindClusters(mice_data, resolution = 0.5)
+#UMAP
+mice_data <- RunUMAP(mice_data, dims = 1:10)
+DimPlot(mice_data, reduction = "umap")
+#Finding Markers:
+mice_data.markers <- FindAllMarkers(mice_data, only.pos =  TRUE, min.pct = 0.25, logfc.threshold = 0.25)
+x <- mice_data.markers %>% group_by(cluster) %>% top_n(n=1, wt = avg_log2FC)
+plot1 <- FeaturePlot(mice_data, features = x$gene[1:4])
+plot2 <- FeaturePlot(mice_data, features = x$gene[5:8])
+
+plot1
+plot2
+top10 <- mice_data.markers %>% group_by(cluster) %>% top_n(n=1, wt = avg_log2FC)
+top10
+#assigning markers
+new.cluster.id = c('OpCml','PcP4','Gria2','Hmgb2', 'Fabp7', 'Ppp2rb','Meg3', 
+                   'Reln')
+names(new.cluster.id) <- levels(mice_data)
+mice_data <- RenameIdents(mice_data,new.cluster.id)
+DimPlot(mice_data, reduction = "umap", label = "True")
+
+head(mice_data)
