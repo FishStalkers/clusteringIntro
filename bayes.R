d590b9f2275c3297995a8abcd19862f701891964
danlee2002 <danlee@gatech.edu>
2023-04-11 17:11:08 -0400

    updated plots

--- bayes.R
+++ bayes.R
@@ -3,9 +3,14 @@ pacman::p_load(dplyr, Seurat, patchwork,bnlearn,Rgraphviz, gRain,sctree)
 mice.data <- Read10X(data.dir = "raw_gene_bc_matrices/mm10/")
 mice_data <- CreateSeuratObject(counts = mice.data, project  ="mice_data", 
                                 min.cells = 3, min.features = 200)
-val = mice_data[c("Hba-a1","Hba-a2", "Hbb-bt" ,"Hbb-bs" ,"Reln" ,  "Alas2" , 
-                  "Ube2c" , "Fabp7" , "Tac2" ,  "Cks2"  )]
-df <- as.data.frame(mice_data@assays$RNA@counts
+
+genesVar = c("Hba-a1","Hba-a2", "Hbb-bt" ,"Hbb-bs" ,"Reln" ,  "Alas2" , 
+          "Ube2c" , "Fabp7" , "Tac2" ,  "Cks2")
+mice_data <- mice_data[genesVar]
+print(dim(mice_data))
+df <- as.data.frame(mice_data, genes = genesVar)
+print(colnames(df))
+df <-df[1:10]
 res <- hc(df)
 plot(res)
 
